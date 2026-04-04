f701cd8f8a7162da39fda2a1df78935016a4d1d1
Anu <anindita.salian@gmail.com>
2026-04-04 12:50:41 +0400

    added models, views, security

---
+++ capy&co/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Capy&Co",
+    "depends": ["base"],
+    "application": True,
+    "data": [
+        "security/ir.model.access.csv",
+        "views/capy_product_views.xml",
+        "views/capy_menus.xml",
+    ]
+}
